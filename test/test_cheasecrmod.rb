fddf34149cab3db4f9e394c5b89790abfaca92c0
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2014-06-25 02:50:18 +0100

    --Now added teardown

--- test/test_cheasecrmod.rb
+++ test/test_cheasecrmod.rb
@@ -15,6 +15,9 @@ class TestCheasecrmod < Test::Unit::TestCase
     #FileUtils.mv('rake_test_defaults.rb', @runner.run_class.rcp.user_defaults_location)
 		if ENV['CHEASE_EXEC']
       CodeRunner.submit(Y: 'test/solovev', p: '{ap: [0.3,0.5,0.4,0.0,0.4,0.0,0.0], at: [0.3,0.5,0.4,0.0,0.4,0.0,0.0]}',  T: false, D: 'rake_test', n: '1', X: ENV['CHEASE_EXEC'])
+    FileUtils.rm(@runner.run_class.rcp.user_defaults_location + '/rake_test_defaults.rb')
+    FileUtils.rm('test/solovev/rake_test_defaults.rb')
+		FileUtils.rm_r('test/solovev/v')
 		end
       #CodeRunner.submit(Y: 'test/ifspppl', T: false, D: 'rake_test', n: '1', X: ENV['TRINITY_EXEC'], p: '{restart_id: 1}')
 			#Dir.chdir('test/ifspppl') do
@@ -34,7 +37,9 @@ class TestCheasecrmod < Test::Unit::TestCase
 		#test_hash = @runner.run_class.parse_input_file('test/ifspppl/v/id_1/v_id_1_t.trin')
 		#assert_equal(base_hash, test_hash)
 		#CodeRunner.status(Y: 'test/ifspppl')
-    #FileUtils.rm(@runner.run_class.rcp.user_defaults_location + '/rake_test_defaults.rb')
-    #FileUtils.rm('test/ifspppl/rake_test_defaults.rb')
+  end
+  def teardown
+    FileUtils.rm('test/solovev/.code_runner_script_defaults.rb')
+    FileUtils.rm('test/solovev/.CODE_RUNNER_TEMP_RUN_LIST_CACHE')
   end
 end
